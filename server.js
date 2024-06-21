a2047e0f543d332c061476bc49a9e9487cc8d3f0
Admin11052003 <giangtra123457@gmail.com>
2024-06-21 22:34:17 +0700

    last deploy

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
 //npm init -y
-//npm i express dotenv cors 
+//npm i express dotenv cors
 //npm i -D  @babel/core @babel/node @babel/preset-env
 //npm install --save sequelize mysql2
 //npx sequelize-cli init
 //npx sequelize-cli model:generate --name User --attributes firstName:string,lastName:string,email:string
 //npx sequelize db:migrate
 
-
-import express from 'express'
-require ('dotenv').config()
-import cors from 'cors'
-import initRoutes from './src/routes'
-import connectDatabase from './src/config/connectDatabase'
+import express from "express";
+require("dotenv").config();
+import cors from "cors";
+import initRoutes from "./src/routes";
+import connectDatabase from "./src/config/connectDatabase";
 // import generateDate from './src/ultis/generateDate'
 
-
 // console.log (getNumberFromString('4.2 trieu'))
-const app = express()
-
+const app = express();
 
 // console.log(generateDate());
 
-app.use(cors({
-    origin: "*", 
-    methods: ["POST", "GET", "PUT", "DELETE"]
-}))
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-initRoutes(app)
-connectDatabase()
+initRoutes(app);
+connectDatabase();
 
-
-const port = process.env.PORT || 8888
+const port = process.env.PORT || 8888;
 const listener = app.listen(port, () => {
-    console.log (`Server is running on the port ${listener.address().port}`)
-})
+  console.log(`Server is running on the port ${listener.address().port}`);
+});
